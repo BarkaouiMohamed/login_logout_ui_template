85f7a0fb5f0663e573e16a276c29511005a5b2e8
BarkaouiMohamed <mohamedbarkaoui999@gmail.com>
2022-03-21 10:46:00 +0100

    LoginLogout

---
+++ README.md
@@ -0,0 +1,5 @@
+# ReactLoginSignupUITemplate
+
+Build React Login & Register UI Template with Bootstrap 4 - Create React authentication system such as login and Register template using Bootstrap 4.
+
+[Build React Login & Register UI Template with Bootstrap 4](https://www.positronx.io/build-react-login-sign-up-ui-template-with-bootstrap-4/)
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <div style={{  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "110%"}}><img src="https://i.ibb.co/bPrcwNw/pepolls.png"></img></div>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Register</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default App;
